48827eca9348edac40fd779f14959e3be141dfdb
Ajith PM <ajithpmdwaraka@gmail.com>
2024-06-02 22:24:22 +0530

    Final Commit2.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,45 @@ import Axios from "axios";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [generatedExcuse, setGeneratedExcuse] = useState("");
+  const [users, setUsers] = useState([]);
 
-  const fetchExcuse = (excuse) => {
-    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
+  const fetchUsers = () => {
+    Axios.get("https://665b30b1003609eda4601b5e.mockapi.io/user/User").then(
       (res) => {
-        setGeneratedExcuse(res.data[0].excuse);
+        setUsers(res.data);
       }
     );
   };
 
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div className="App">
-      <h1> Generate An Excuse </h1>
-      <button onClick={() => fetchExcuse("party")}> Party</button>
-      <button onClick={() => fetchExcuse("family")}> Family</button>
-      <button onClick={() => fetchExcuse("office")}> Office </button>
-
-      <p> {generatedExcuse} </p>
+      <h1>User List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Avatar</th>
+            <th>Created At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>
+                <img src={user.avatar} alt={user.name} width="50" height="50" />
+              </td>
+              <td>{user.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
